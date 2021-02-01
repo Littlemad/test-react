01217a31b72efddaaaa5636b96cbcda80cde6fe4
Alessio Carone <alessio.carone@travelport.com>
2021-02-01 20:27:01 +0100

    changes

--- src/common/component/Main.js
+++ src/common/component/Main.js
@@ -3,16 +3,37 @@ import { useHistory } from "react-router-dom";
 
 const main = () => {
   const history = useHistory();
+  const [storage, setStorage] = React.useState(
+    localStorage.getItem("localStorageInput") || ""
+  );
 
   useEffect(() => {
     console.log("main effect");
-  });
+    localStorage.setItem("localStorageInput", storage);
+  }, [storage]);
+
   console.log("main");
+
+  const handleOnChange = (e) => {
+    setStorage(e.target.value);
+  };
 
   return (
     <>
       <h1>Main</h1>
-      <button onClick={() => history.push("/head")}>click me</button>
+      <button onClick={() => history.push("/head")}>Head</button>
+      <br />
+      <br />
+      <form>
+        <label>Name</label>
+        <input
+          name="name"
+          value={storage}
+          type="text"
+          onChange={handleOnChange}
+        />
+      </form>
+      <button onClick={() => setStorage("Pippo")}>Pippo</button>
     </>
   );
 };
